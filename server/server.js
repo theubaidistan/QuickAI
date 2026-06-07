277c97571f179b7720347b0810012b51a40a7833
theubaidistan <ubaidistan395@gmail.com>
2026-06-07 13:27:29 +0500

    Refactor index.html title and add server index.html and favicon.svg

--- server/server.js
+++ server/server.js
@@ -5,8 +5,17 @@ import { clerkMiddleware, requireAuth } from "@clerk/express";
 import aiRouter from "./routes/aiRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import userRouter from "./routes/userRoutes.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
+
+// ESM equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files (favicon, images, etc.)
+app.use(express.static(path.join(__dirname, "public")));
 
 await connectCloudinary();
 
@@ -14,7 +23,12 @@ app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
-app.get("/", (req, res) => res.send("Server is Live!"));
+// app.get("/", (req, res) => res.send("Server is Live!"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.use(requireAuth());
 app.use("/api/ai", aiRouter);
 app.use("/api/user", userRouter);
