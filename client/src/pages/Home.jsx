e9fcad37cb89803f04a5be1605069bb5e08dae01
theubaidistan <ubaidistan395@gmail.com>
2026-06-07 12:56:26 +0500

    Fix import case for Navbar component in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AiTools from "../components/AiTools";
 import Testimonial from "../components/Testimonial";
